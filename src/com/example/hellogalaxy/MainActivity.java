77da3c4c69c67c572ce7d8c9308c1c95951c1bb2
student <student@PC111S09.fgakusei.local>
2014-05-22 14:18:04 +0900

    ok

---
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.hellogalaxy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+public void onClick(View v) {
+	// TODO 自動生成されたメソッド・スタブ
+
+}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
