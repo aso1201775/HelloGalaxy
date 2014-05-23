d2ee217e527562631ddf3558f74e064869587f3a
student <student@PC111S09.fgakusei.local>
2014-05-23 10:34:26 +0900

    jijiji

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 @Override
@@ -13,9 +15,24 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	}
 
 	@Override
+protected void onResume() {
+	// TODO 自動生成されたメソッド・スタブ
+	super.onResume();
+	// ボタンをIDで探してボタン変数をつくる
+	Button btnOK = (Button)findViewById(R.id.btnOK);
+	// ボタン変数にリスナーを登録する
+	btnOK.setOnClickListener(this);
+}
+
+	@Override
 public void onClick(View v) {
 	// TODO 自動生成されたメソッド・スタブ
-
+		switch(v.getId()){
+		case R.id.btnOK: //btnOKが押されたら
+			// エディットテキストから入力内容を取り出す
+			EditText etv = (EditText)findViewById(R.id.edtFirst);
+				String inputMsg = etv.getText().toString();
+		}
 }
 
 	@Override
